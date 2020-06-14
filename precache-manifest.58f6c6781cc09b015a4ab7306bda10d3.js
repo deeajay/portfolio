3aab62072d4fea826a767ed9fd5c42ea49853123
Ajay Deekonda <ajay@rockmetric.com>
2020-06-14 13:38:40 +0530

    Updates

---
+++ precache-manifest.58f6c6781cc09b015a4ab7306bda10d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f7a63d13421580abcd7bdcd8ac4bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a104c26e4e671c6f4c",
+    "url": "/static/js/2.c4159323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4159323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84523cf8516a511a4c7",
+    "url": "/static/js/main.c5510ccb.chunk.js"
+  },
+  {
+    "revision": "3cac72df019adf119d43",
+    "url": "/static/js/runtime-main.0f04c299.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f1000955e0f6d6a0fcf9eb2524b91b16.js"
+  "/precache-manifest.58f6c6781cc09b015a4ab7306bda10d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
