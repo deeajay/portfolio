fc5e55ff348b370a6327653b70867a86cd0c02b8
Ajay Deekonda <ajay@rockmetric.com>
2020-06-14 19:12:00 +0530

    updates

---
+++ src/Sections.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import configData from './config.json';
+
+class Sections extends Component {
+    render() {
+
+        let SectionContent = configData.sections.map(function (innerData, index) {
+
+            let actualContent = null;
+
+            switch (innerData.type) {
+                case "numbered":
+                    actualContent = innerData.content.map(function (contentData, index) {
+                        return (
+                            <div class="o-grid__col-sm-6">
+                                <div class="o-content">
+                                    <div class="o-media  o-media--block">
+                                        {
+                                            contentData.number ?
+                                                <div class="o-media__figure">
+                                                    <div class="c-number  t-primary-color">
+                                                        {contentData.number}{contentData.percentage ? <small>%</small> : null}
+                                                    </div>
+                                                </div> : null
+                                        }
+                                        <div class="o-media__body  o-content__body">
+                                            <h3>{contentData.header}</h3>
+                                            {contentData.content ? <p>{contentData.content}</p> : null}
+                                        </div>
+                                    </div>
+                                    {
+                                        contentData.percentage ?
+                                            <div class="c-progress-bar  o-content__body  t-border-color-bg  u-mt-text">
+                                                <div class="a-progress-bar  c-progress-bar__filler  t-primary-bg" data-percent="80"></div>
+                                            </div> : null
+                                    }
+                                </div>
+                            </div>
+                        )
+                    })
+                    actualContent = <div class="o-section__content  t-section__content  ">
+                        <div class="o-grid">
+                            {actualContent}
+                        </div>
+                    </div>;
+                    break;
+                case "para":
+                    actualContent = innerData.content.map(function (contentData, index) {
+                        return (<p>{contentData.content}</p>)
+                    });
+                    actualContent = <div class="o-section__content t-section__content">
+                        <div class="o-content">
+                            <div class="c-intro">
+                                <div class="o-content__body">
+                                    {actualContent}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    break;
+            }
+
+            return (
+                <section className="o-section t-section" key={index}>
+                    <div className="o-section__header-bg  t-section__header"></div>
+                    <div className="o-section__content-bg  t-section__content"></div>
+
+                    <div className="o-container">
+                        <div className="o-section__container">
+
+                            <header className="o-section__header  t-section__header">
+                                <div className="o-content">
+                                    <h2 className="o-section__heading">
+                                        {innerData.header.name}
+                                    </h2>
+                                    <div className="o-content__body  o-section__description">
+                                        {innerData.header.tag}
+                                    </div>
+                                </div>
+                            </header>
+
+                            {actualContent}
+
+                        </div>
+                    </div>
+
+                </section>
+            )
+        });
+
+        return (
+            <>
+                {SectionContent}
+            </>
+        );
+    }
+
+}
+
+export default Sections;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from "./Container";
 import Footer from "./Footer";
+import Sections from "./Sections";
 
 ReactDOM.render(
     <>
         <Container />
+        <Sections />
         <Footer />
     </>
     , document.getElementById('root'));
